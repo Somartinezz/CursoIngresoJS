0bcefadfe939c443247c35bd9e80ab9343d6755a
Sofia Martinez <sso.martinezz@gmail.com>
2022-02-11 20:00:14 -0300

    nuevos cambios

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,19 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+// devuelve un dato y se guarda en la variable
+//variable : var 
+//intelliSense: opcion de autocompletado
+//camelcase:nomeclatura
+//todo lo literal va entre comillas 
+// asignacion = una cosa en otra de derecha a ziquierda
 function mostrar()
-{
-	alert("ok");
-}
 
+
+{
+	var nombreDelAlumno;
+
+	nombreDelAlumno = prompt("ingrese su nombre!"); //asignacion 
+	
+	alert(nombreDelAlumno);
+}
